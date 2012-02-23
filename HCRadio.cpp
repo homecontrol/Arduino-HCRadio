f7949b2a09c1afd6100e8e436504f5df11bdef1f
Daniel Morlock <daniel.morlock@gmail.com>
2012-02-24 00:09:57 +0100

    Fixed low memory problem, fixed sending of raw data.

--- HCRadio.cpp
+++ HCRadio.cpp
@@ -5,95 +5,95 @@ volatile HCRadioResult hcradio_result;
 HCRadio::HCRadio()
 :status_pin(-1), irq(-1), pulse_length(-1), send_pin(-1), send_repeat(-1)
 {
-	hcradio_result.ready = false;
-	hcradio_result.pulse_length = 0;
-	hcradio_result.len_timings = 0;
-	hcradio_result.last_time = 0;
-	hcradio_result.repeat_count = 0;
-	hcradio_result.change_count = 0;
+    hcradio_result.ready = false;
+    hcradio_result.pulse_length = 0;
+    hcradio_result.len_timings = 0;
+    hcradio_result.last_time = 0;
+    hcradio_result.repeat_count = 0;
+    hcradio_result.change_count = 0;
 }
 
 void HCRadio::enable_receive(int irq)
 {
-	this->irq = irq;
-	attachInterrupt(this->irq, HCRadio::receive_interrupt, CHANGE);
+    this->irq = irq;
+    attachInterrupt(this->irq, HCRadio::receive_interrupt, CHANGE);
 }
 
 void HCRadio::disable_receive()
 {
-	detachInterrupt(this->irq);
-	this->irq = -1;
+    detachInterrupt(this->irq);
+    this->irq = -1;
 }
 
 void HCRadio::enable_send(int pin, int repeat, int pulse_length)
 {
-	this->send_repeat = repeat;
-	this->send_pin = pin;
-	this->pulse_length = pulse_length;
+    this->send_repeat = repeat;
+    this->send_pin = pin;
+    this->pulse_length = pulse_length;
 }
 
 void HCRadio::disable_send()
 {
-	this->send_repeat = -1;
-	this->send_pin = -1;
-	this->pulse_length = -1;
+    this->send_repeat = -1;
+    this->send_pin = -1;
+    this->pulse_length = -1;
 }
 
 void HCRadio::enable_status(int pin)
 {
-	this->status_pin = pin;
-	pinMode(pin, OUTPUT);
+    this->status_pin = pin;
+    pinMode(pin, OUTPUT);
 }
 
 void HCRadio::disable_status()
 {
-	status_pin = -1;
+    status_pin = -1;
 }
 
 void HCRadio::set_pulse_length(int pulse_length)
 {
-	this->pulse_length = pulse_length;
+    this->pulse_length = pulse_length;
 }
 
 void HCRadio::set_send_repeat(int repeat)
 {
-	this->send_repeat = repeat;
+    this->send_repeat = repeat;
 }
 
 void HCRadio::receive_interrupt()
 {
-	unsigned long time = micros();
-	unsigned long duration = time - hcradio_result.last_time;
+    unsigned long time = micros();
+    unsigned int duration = time - hcradio_result.last_time;
 
-	if(duration > 5000 &&
-	   duration > hcradio_result.timings[0] - 200 &&
-	   duration < hcradio_result.timings[0] + 200)
-	{
-		hcradio_result.repeat_count ++;
-		hcradio_result.change_count --; // ??
+    if(duration > 5000 &&
+            duration > hcradio_result.timings[0] - 200 &&
+            duration < hcradio_result.timings[0] + 200)
+    {
+        hcradio_result.repeat_count ++;
+        hcradio_result.change_count --; // ??
 
-		if(hcradio_result.repeat_count == 2)
-		{
-			hcradio_result.ready = true;
-			hcradio_result.len_timings = hcradio_result.change_count;
-			hcradio_result.repeat_count = 0;
-		}
+        if(hcradio_result.repeat_count == 2)
+        {
+            hcradio_result.ready = true;
+            hcradio_result.len_timings = hcradio_result.change_count;
+            hcradio_result.repeat_count = 0;
+        }
 
-		hcradio_result.change_count = 0;
-	}
-	else if(duration > 5000)
-	{
-		hcradio_result.change_count = 0;
-	}
+        hcradio_result.change_count = 0;
+    }
+    else if(duration > 5000)
+    {
+        hcradio_result.change_count = 0;
+    }
 
-	if(hcradio_result.change_count >= HCRADIO_MAX_TIMINGS)
-	{
-		hcradio_result.change_count = 0;
-		hcradio_result.repeat_count = 0;
-	}
+    if(hcradio_result.change_count >= HCRADIO_MAX_TIMINGS)
+    {
+        hcradio_result.change_count = 0;
+        hcradio_result.repeat_count = 0;
+    }
 
-	hcradio_result.timings[hcradio_result.change_count ++] = duration;
-	hcradio_result.last_time = time;
+    hcradio_result.timings[hcradio_result.change_count ++] = duration;
+    hcradio_result.last_time = time;
 }
 
 void HCRadio::send_0()
@@ -104,65 +104,126 @@ void HCRadio::send_0()
 
 void HCRadio::send_1()
 {
-	this->transmit(3,1);
-	this->transmit(3,1);
+    this->transmit(3,1);
+    this->transmit(3,1);
 }
 
 void HCRadio::send_f()
 {
-	this->transmit(1,3);
-	this->transmit(3,1);
+    this->transmit(1,3);
+    this->transmit(3,1);
 }
 
 void HCRadio::sync()
 {
-	this->transmit(1,31);
+    this->transmit(1,31);
 }
 
 void HCRadio::transmit(int high, int low)
 {
-	digitalWrite(send_pin, HIGH);
-	delayMicroseconds(pulse_length * high);
-	digitalWrite(send_pin, LOW);
-	delayMicroseconds(pulse_length * low);
+    digitalWrite(send_pin, HIGH);
+    delayMicroseconds(pulse_length * high);
+    digitalWrite(send_pin, LOW);
+    delayMicroseconds(pulse_length * low);
 }
 
 bool HCRadio::send_raw(unsigned long* timings, unsigned int len_timings)
 {
-	if(this->status_pin != -1)
-		digitalWrite(this->status_pin, HIGH);
+    if(this->status_pin != -1)
+        digitalWrite(this->status_pin, HIGH);
 
-	noInterrupts();
-	cli();
+    noInterrupts();
+    cli();
 
-	this->sync();
-	for(int i = 0; i < this->send_repeat; i ++)
-	{	
-		for(unsigned int j = 0; j < len_timings; j ++)
-		{
-			if((j % 2) == 0) digitalWrite(this->send_pin, HIGH);
-			else digitalWrite(this->send_pin, LOW);
-			delayMicroseconds(timings[j]);
-		}
+    this->sync();
+    for(int i = 0; i < this->send_repeat; i ++)
+    {
+        for(unsigned int j = 0; j < len_timings; j ++)
+        {
+            if((j % 2) == 0) digitalWrite(this->send_pin, HIGH);
+            else digitalWrite(this->send_pin, LOW);
+            delayMicroseconds(timings[j]);
+        }
 
-		this->sync();
-	}
+        this->sync();
+    }
 
-	interrupts();
-	sei();
+    interrupts();
+    sei();
 
-	if(this->status_pin != -1)
-		digitalWrite(this->status_pin, LOW);
+    if(this->status_pin != -1)
+        digitalWrite(this->status_pin, LOW);
 
-	return true;
+    return true;
+}
+
+bool HCRadio::send_raw(const char* code)
+{
+    if(this->status_pin != -1)
+        digitalWrite(this->status_pin, HIGH);
+
+    noInterrupts();
+    cli();
+
+    this->sync();
+    int t = 0;
+    for(int i = 0; i < this->send_repeat; i ++)
+    {
+        t = 0;
+        int j = 0;
+        const char* c = code;
+        while(true)
+        {
+            if(*c == '.' || *c == 0)
+            {
+                if((j % 2) == 0) digitalWrite(this->send_pin, HIGH);
+                else digitalWrite(this->send_pin, LOW);
+                delayMicroseconds(t);
+
+                if(*c == 0)
+                    break;
+
+                j ++;
+                t = 0;
+            }
+            else
+            {
+                if(*c < '0' || *c > '9')
+                {
+                    t = -1;
+                    break;
+                }
+
+                t = 10 * t + (*c - '0');
+            }
+
+            c ++;
+        }
+
+        if(t < 0)
+            break;
+
+        this->sync();
+    }
+
+    interrupts();
+    sei();
+
+    if(this->status_pin != -1)
+        digitalWrite(this->status_pin, LOW);
+
+    if(t < 0)
+        return false;
+
+    return true;
 }
 
 bool HCRadio::send_tristate(char* code)
 {
-	if(status_pin != -1)
-		digitalWrite(status_pin, HIGH);
+    if(status_pin != -1)
+        digitalWrite(status_pin, HIGH);
 
-	bool valid_chars = true;
+    bool valid_chars = true;
 
     char* c = code;
     while (*c)
@@ -177,55 +238,55 @@ bool HCRadio::send_tristate(char* code)
 
     if (valid_chars)
     {
-    	noInterrupts();
-    	cli();
+        noInterrupts();
+        cli();
 
-    	this->sync();
-    	for(int i = 0; i < this->send_repeat; i ++)
-    	{
-    		c = code;
-			while (*c)
-			{
-				if (*c == '0') this->send_0();
-				else if (*c == '1') this->send_1();
-				else this->send_f();
+        this->sync();
+        for(int i = 0; i < this->send_repeat; i ++)
+        {
+            c = code;
+            while (*c)
+            {
+                if (*c == '0') this->send_0();
+                else if (*c == '1') this->send_1();
+                else this->send_f();
 
-				c += 1;
-			}
+                c += 1;
+            }
 
-			this->sync();
-    	}
+            this->sync();
+        }
 
-    	interrupts();
-    	sei();
+        interrupts();
+        sei();
     }
 
-	if(status_pin != -1)
-		digitalWrite(status_pin, LOW);
+    if(status_pin != -1)
+        digitalWrite(status_pin, LOW);
 
-	return valid_chars;
+    return valid_chars;
 }
 
 bool HCRadio::decode(HCRadioResult* result)
 {
-	noInterrupts();
-	if(hcradio_result.ready == false)
-	{
-		interrupts();
-		return false;
-	}
+    noInterrupts();
+    if(hcradio_result.ready == false)
+    {
+        interrupts();
+        return false;
+    }
 
-	result->len_timings = hcradio_result.len_timings;
-	result->last_time = hcradio_result.last_time;
+    result->len_timings = hcradio_result.len_timings;
+    result->last_time = hcradio_result.last_time;
 
-	for(unsigned int i = 0; i < result->len_timings; i ++)
-		result->timings[i] = hcradio_result.timings[i];
+    for(unsigned int i = 0; i < result->len_timings; i ++)
+        result->timings[i] = hcradio_result.timings[i];
 
-	hcradio_result.ready = false;
-	interrupts();
+    hcradio_result.ready = false;
+    interrupts();
 
-	if(result->len_timings > 0)
-		result->pulse_length = result->timings[0] / 31;
+    if(result->len_timings > 0)
+        result->pulse_length = result->timings[0] / 31;
 
-	return true;
+    return true;
 }
--- HCRadio.h
+++ HCRadio.h
@@ -1,9 +1,6 @@
 #ifndef HCRADIO_H_
 #define HCRADIO_H_
 
-
-// TODO: Change return value of HCRadioResult::get_json() into char* to remove this!
-
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
@@ -16,52 +13,51 @@
 
 class HCRadioResult
 {
-	public:
+    public:
 
-		bool ready;
-		unsigned long pulse_length;
-		unsigned long timings[HCRADIO_MAX_TIMINGS];
-		unsigned int len_timings;
-		unsigned long last_time;
-		unsigned int repeat_count;
-		unsigned int change_count;
+        bool ready;
+        unsigned int pulse_length;
+        unsigned int timings[HCRADIO_MAX_TIMINGS];
+        unsigned int len_timings;
+        unsigned long last_time;
+        unsigned int repeat_count;
+        unsigned int change_count;
 };
 
 class HCRadio
-
 {
-	public:
+    public:
 
-		HCRadio();
+        HCRadio();
 
-		void enable_status(int pin);
-		void disable_status();
-		void enable_receive(int irq);
-		void disable_receive();
-		void enable_send(int pin, int repeat = 10, int pulse_length = 380);
-		void disable_send();
-		void set_pulse_length(int pulse_length);
-		void set_send_repeat(int repeat);
-		void send_0();
-		void send_1();
-		void send_f();
-		void sync();
-		bool send_tristate(char* code);
-		bool send_raw(unsigned long* timings, unsigned int len_timings);
+        void enable_status(int pin);
+        void disable_status();
+        void enable_receive(int irq);
+        void disable_receive();
+        void enable_send(int pin, int repeat = 10, int pulse_length = 380);
+        void disable_send();
+        void set_pulse_length(int pulse_length);
+        void set_send_repeat(int repeat);
+        void send_0();
+        void send_1();
+        void send_f();
+        void sync();
+        bool send_tristate(char* code);
+        bool send_raw(unsigned long* timings, unsigned int len_timings);
+        bool send_raw(const char* c);
 
-		bool decode(HCRadioResult* result);
+        bool decode(HCRadioResult* result);
 
-	private:
+    private:
 
-		int status_pin;
-		int irq;
-		int pulse_length;
-		int send_pin;
-		int send_repeat;
+        int status_pin;
+        int irq;
+        int pulse_length;
+        int send_pin;
+        int send_repeat;
 
-		void transmit(int high, int low);
-
-		static void receive_interrupt();
+        void transmit(int high, int low);
+        static void receive_interrupt();
 };
 
 #endif
